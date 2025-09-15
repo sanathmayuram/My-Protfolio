9ee75af4c4e2565f21faeee09d416b674332ddc5
sanathmayuram <ramakrishnasanathmayuram@gmail.com>
2025-09-15 18:47:26 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+    navLinks.classList.remove("show");
+  });
+});
